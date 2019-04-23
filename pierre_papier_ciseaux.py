9241affbb7e9e381b9cdf68c244a565033be7771
RogerNoel <noel.roger.pro@gmail.com>
2019-04-23 08:47:03 +0200

    correction nom fichier

--- pierre_papier_ciseaux.py
+++ pierre_papier_ciseaux.py
@@ -5,7 +5,6 @@ from random import *
 #inviter l'utilisateur
 game = ""
 game = input('Voulez-vous jouer à Pierre-Papier-Ciseaux ? \nEntrez "O" ou "N".')
-print(game)
 #gérer le mauvais choix de validation acceptation
 while not (game == "O" or game=="N"):
     print('Veuillez entrer "O" ou "N".')
@@ -17,35 +16,35 @@ if (game == "O"):
         choixUt = input("Faites votre choix entre pierre, papier et ciseaux : ")
     #génerer aléatoirement un ppc de l'AI
     x = randint (1,3)
-    AIChoice = ""
+    choixAI = ""
     if (x==1):
-        AIChoice = "pierre"
+        choixAI = "pierre"
     elif (x==2):
-        AIChoice = "papier"
+        choixAI = "papier"
     else:
-        AIChoice = "ciseaux"
+        choixAI = "ciseaux"
 
     #gérer l'égalité
-    if (choixUt == AIChoice):
+    if (choixUt == choixAI):
         print("Egalité")
     else: #comparer l'entrée et le choix AI
         if (choixUt == "pierre"):
-            if (AIChoice == "papier"):
+            if (choixAI == "papier"):
                 print('L\'AI a choisi "papier", vous avez perdu')
             else:
                 print('L\'AI a choisi "ciseaux", vous avez gagné')
         if (choixUt == "papier"):
-            if (AIChoice == "pierre"):
+            if (choixAI == "pierre"):
                 print('L\'AI a choisi "pierre", vous avez gagné')
             else:
                 print('L\'AI a choisi "ciseaux", vous avez perdu')
         if (choixUt == "ciseaux"):
-            if(AIChoice == "pierre"):
+            if(choixAI == "pierre"):
                 print('L\'AI a choisi "ciseaux", vous avez gagné')
             else:
                 print('L\'AI a choisi "papier", vous avez perdu')
 else: #gérer le refus utilisateur
-    print("Ce sera pour une autre fois alors !\A bientôt")
+    print("Ce sera pour une autre fois alors !\nA bientôt")
 
 
 
