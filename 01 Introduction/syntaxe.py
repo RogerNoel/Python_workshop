505082566b84b11de241c7d2ab728e6319429bb8
RogerNoel <noel.roger.pro@gmail.com>
2019-04-25 13:59:38 +0200

    shit commit

--- 01 Introduction/syntaxe.py
+++ 01 Introduction/syntaxe.py
@@ -6,8 +6,8 @@ message = « Welcome to our workshop »
 
 # Affichage avec print()
 
-#bloc conditionnel : l’utilisation des deux points « : » conjuguée à l’indentation permet de délimiter les blocs :
-
+# bloc conditionnel : l’utilisation des deux points « : » 
+# conjuguée à l’indentation permet de délimiter les blocs :
 a = 0
 if a > 0:
     print(« a est positif »)
@@ -17,7 +17,6 @@ else :
     print(« a est nul »)
 
 # de la même manière, le système des blocs pour un while
-
 a = 0
 while (a < 7):
     a = a + 1
